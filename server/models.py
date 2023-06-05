7ebc77cc030173093d51130281e08acd6a16606b
Luna Daniels <aleldaproductions@gmail.com>
2023-06-05 14:04:23 -0600

    added password hash stuff to models.py

--- server/models.py
+++ server/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 import re
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
@@ -10,7 +11,7 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String)
+    _password_hash = db.Column(db.String)
     profile_picture = db.Column(db.String)
     bio = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
@@ -53,6 +54,17 @@ class User(db.Model, SerializerMixin):
             raise ValueError("Password must contain at least one special character.")
         return password
 
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
 
 class Post(db.Model, SerializerMixin):
     __tablename__ = 'posts'
