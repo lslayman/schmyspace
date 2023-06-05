cede92ebab6fc9ba7b6503455b378c275a9901bb
Luna Daniels <aleldaproductions@gmail.com>
2023-06-05 10:03:33 -0600

    added columns to post and message

--- server/models.py
+++ server/models.py
@@ -11,11 +11,28 @@ class Post(db.Model, SerializerMixin):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String)
+    content = db.Column(db.String)
+    title = db.Column(db.String)
+    comments = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String)
+    content = db.Column(db.String)
+    status = db.Column(db.String)
+    attachments = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    friend_id = db.Column(db.Integer, db.ForeignKey('friends.id'))
 
 class Friend(db.Model, SerializerMixin):
     __tablename__ = 'friends'
