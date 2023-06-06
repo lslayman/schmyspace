a0b9100fa19dca501d25d6cb97dfdb4d0c382ea0
Luna Daniels <aleldaproductions@gmail.com>
2023-06-06 16:25:10 -0600

    functional login logout

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,59 @@
+import { useState, useEffect } from 'react'
+
 function Login(){
-    return(
-        true
-    )
+    const [user, setUser] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch('/api/login', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({username: username, password: password})
+        })
+        .then(res => res.json())
+        .then(data => setUser(data))
+    }
+
+    function handleLogout(e){
+        e.preventDefault()
+        fetch('/api/logout', {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json"}
+        })
+        .then(setUser(null))
+    }
+
+    function handleUsername(e){
+        setUsername(e.target.value)
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value)
+    }
+    if(user){
+        return(
+            <div>
+                <form onSubmit={handleLogout}>
+                    <button type='submit'>Logout</button>
+                </form>
+            </div>
+        )
+    }
+    else{
+        return(
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' value={username} placeholder="Username" onChange={handleUsername} />
+                    <br></br>
+                    <input type='text' value={password} placeholder="Password" onChange={handlePassword}/>
+                    <br></br>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Login
