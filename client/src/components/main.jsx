7ab0511bf36fd2eeeaf4052ce8867b813ed56497
Luna Daniels <aleldaproductions@gmail.com>
2023-06-06 11:05:10 -0600

    added main.jsx

---
+++ client/src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+)
