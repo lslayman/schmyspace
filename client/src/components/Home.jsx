3ca327f58fa11c016d997532e1275d1dfb0a73a4
lslayman <laurenaslayman@gmail.com>
2023-06-10 05:34:34 -0600

    adds readme

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,6 +10,10 @@ function Home({setUsers}){
             <h1 class="centered-text">schmyspace</h1>
             <p class="centered text">we won't radicalize your grandparents</p>
         </div>
+        <div className="welcome">
+            <h2>Welcome to schmyspace!</h2>
+            <p>If you've been missing a time of yore, when you could get on social media to make friends, watch "Charlie the Unicorn", out-scene the scenecore kids, and brag about being straight-edge but still a force to be reckoned with in the mosh pit, look no further, schmyspace is here! Though still a work in progress, our little site was inspired by a long-lost social media platform that existed in the good-old days when the internet almost felt quaint and you didn't have to worry about algorithms clogging your feed with garbage, billionaires selling your private information to hostile foreign governments. Kick up your feet and stay a while! We don't have all the bells and whistles just yet, but we promise not to radicalize your grandparents. </p>
+        </div>
         </>
     )
 }
